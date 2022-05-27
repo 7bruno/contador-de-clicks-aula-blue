c4605923ee8224691930fcff7f0f8e7a3deb45be
Bruno Alexandre Gomes de souza <bruno.souza@fast2mine.com>
2022-05-26 23:03:13 -0300

    Only click counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,18 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
-import { ButtonComponent } from './components/button-component';
 
 function App() {
   const [clicks, setClicks] = useState(0);
-
-  useEffect(()=>{console.log('effect')}, [clicks]);
 
   const addClicks = ()=>{
     setClicks(clicks+1);
     console.log(clicks);
   }
 
-  const subClicks = ()=>{
-    setClicks(clicks-1);
-    console.log(clicks);
-  }
-
   return (
     <div className="App">
-      <ButtonComponent onClick={addClicks} >Adicionar</ButtonComponent>
-      <ButtonComponent onClick={subClicks} >Subtrair</ButtonComponent>
-      <h1>{clicks!==0?`resumo dos clicks: ${clicks}`:'sem click identificado'}</h1>
+      <button onClick={addClicks} >Adicionar</button>
+      <h1>{clicks}</h1>
     </div>
   );
 }
